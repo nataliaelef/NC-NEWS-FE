b6272be713e1f1c8617e6e34d3dbc4b0c40bad77
Natali Eleftheriou <nataliaelef@outlook.com>
2019-02-05 09:09:39 +0000

    added users list

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,8 +1,29 @@
 import React, { Component } from 'react';
+import * as api from '../utils/api';
 
 class Users extends Component {
+  state = {
+    users: []
+  };
+
+  componentDidMount = async () => {
+    const users = await api.getUsers();
+    this.setState({ users });
+  };
+
   render() {
-    return <div>Users</div>;
+    const { users } = this.state;
+    return (
+      <div className="users">
+        <ul>
+          {users.map(user => (
+            <li className="userList" key={user.name}>
+              {user.name}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
   }
 }
 
