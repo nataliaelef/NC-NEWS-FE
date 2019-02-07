cee2c87d868c1ad2b59c8bfa274fda6ce88ff634
Natali Eleftheriou <nataliaelef@outlook.com>
2019-02-07 23:09:39 +0000

    articles in home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,24 +1,9 @@
 import React, { Component } from 'react';
-// import { Link } from '@reach/router';
-// // import { Card, Form, Input, TextArea, Button } from 'semantic-ui-react';
-// import * as api from '../utils/api';
+import Articles from './Articles';
 
 class Home extends Component {
-  // state = {
-  //   username: ''
-  // };
-  // renderArticles = () => {
-  //   const { username } = this.state;
-  //   this.setState({ username });
-  // };
-  // componentDidMount = async () => {
-  //   const articles = await api.getArticles();
-  //   this.setState({ articles });
-  // };
   render() {
-    console.log(this.props.user);
-    return <div>home</div>;
-    // return <div className="articles" >{this.renderArticles()}</div>;
+    return <Articles />;
   }
 }
 
