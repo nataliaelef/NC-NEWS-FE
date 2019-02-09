6f1d4b01da62bd53bdddf0a0a182c9cca21a7c1f
Natali Eleftheriou <nataliaelef@outlook.com>
2019-02-09 22:18:11 +0000

    Added about in grid

--- src/components/About.js
+++ src/components/About.js
@@ -1,18 +1,32 @@
 import React from 'react';
-import { Header, Segment } from 'semantic-ui-react';
+import {
+  Header,
+  Container,
+  Grid,
+  Segment,
+  GridColumn
+} from 'semantic-ui-react';
 
 const About = () => {
   return (
-    <Segment>
-      <Header as="h1">About</Header>
-      <p>
-        Northcoders news is a webpage where you can read articles regarding
-        several topics. Using the login dropdown you can get access in more
-        functionabilies, such as adding articles and comments, voting on
-        articles and comments. See articles by topic and delete your own
-        articles and comments.
-      </p>
-    </Segment>
+    <Container>
+      <Grid>
+        <Grid.Row>
+          <Header as="h1">About</Header>
+        </Grid.Row>
+        <Grid.Row>
+          <Grid.Column computer={8} mobile={16}>
+            <p>
+              Northcoders news is a webpage where you can read articles
+              regarding several topics. Using the login dropdown you can get
+              access in more functionabilies, such as adding articles and
+              comments, voting on articles and comments. See articles by topic
+              and delete your own articles and comments.
+            </p>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </Container>
   );
 };
 export default About;
