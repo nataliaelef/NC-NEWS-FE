f4892c49e55ed5613e73ca879af9b8333e212055
Natali Eleftheriou <nataliaelef@outlook.com>
2019-02-12 07:33:26 +0000

    fixed error if article not found

--- src/components/Article.js
+++ src/components/Article.js
@@ -37,114 +37,105 @@ class Article extends Component {
     });
   };
 
-  // componentDidMount = async () => {
-  //   const { id } = this.props;
-  //   const article = await api
-  //     .getArticleById(id)
-  //     .then(article => {
-  //       this.setState({ article });
-  //     })
-  //     .catch(err => {
-  //       this.setState({ hasError: true });
-  //     });
-
-  //   const comments = await api.getCommentsByArticleId(id);
-  //   this.setState({ article, comments });
-  // };
-
-  componentDidMount = async () => {
+  componentDidMount = () => {
     const { id } = this.props;
-    const article = await api.getArticleById(id);
-    const comments = await api.getCommentsByArticleId(id);
-    this.setState({ article, comments });
+    api
+      .getArticleById(id)
+      .then(async article => {
+        const comments = await api.getCommentsByArticleId(id);
+        this.setState({ article, comments });
+      })
+      .catch(err => {
+        this.setState({ hasError: true });
+      });
   };
 
   render() {
     const { article, votes, comments, redirect, hasError } = this.state;
     const { id: articleId, user } = this.props;
-    // if (hasError) {
-    //   return <PageNotFound />;
-    // } else {
-    return !redirect ? (
-      <Grid className="ui container">
-        <Grid.Row>
-          <h1 className="ui header">{article.title}</h1>
-        </Grid.Row>
-        <Grid.Row>
-          <Grid.Column floated="right" computer={2} mobile={8}>
-            <Image
-              floated="right"
-              size="mini"
-              src={`https://api.adorable.io/avatars/${Math.round(
-                Math.random() * 1000
-              )}`}
-            />
-            <Header as="h4">{article.author}</Header>
-          </Grid.Column>
-        </Grid.Row>
-        <Grid.Row>
-          <p className="body">{article.body}</p>
-        </Grid.Row>
-        <Grid.Row>
-          <div className="votes">Votes: {article.votes + votes} </div>
-        </Grid.Row>
-        <Grid.Row>
-          {user ? (
-            <Grid.Column computer={8} mobile={8}>
-              <Grid.Row>
-                <Button
-                  basic
-                  color="green"
-                  disabled={votes === 1}
-                  onClick={() => this.handleVoteClick(1)}
-                >
-                  Upvote
-                </Button>
-                <Button
-                  basic
-                  color="red"
-                  disabled={votes === -1}
-                  onClick={() => this.handleVoteClick(-1)}
-                >
-                  Downvote
-                </Button>
-              </Grid.Row>
-            </Grid.Column>
-          ) : (
-            ''
-          )}
-          {user === article.author ? (
-            <Grid.Column computer={8} mobile={8}>
-              <Button
-                primary
+    if (hasError) {
+      return <PageNotFound />;
+    } else {
+      return !redirect ? (
+        <Grid className="ui container">
+          <Grid.Row>
+            <h1 className="ui header">{article.title}</h1>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column floated="right" computer={2} mobile={8}>
+              <Image
                 floated="right"
-                onClick={() => this.handleDelete(articleId)}
-              >
-                Delete Article
-              </Button>
+                size="mini"
+                src={`https://api.adorable.io/avatars/${Math.round(
+                  Math.random() * 1000
+                )}`}
+              />
+              <Header as="h4">{article.author}</Header>
             </Grid.Column>
-          ) : (
-            ''
-          )}
-        </Grid.Row>
-        <Grid.Row>
-          <Grid.Column>
-            <CommentAdder
-              articleId={articleId}
-              postedComment={this.postedComment}
-              user={user}
-            />
-          </Grid.Column>
-        </Grid.Row>
-        <Grid.Row>
-          <Comments comments={comments} user={user} />
-        </Grid.Row>
-      </Grid>
-    ) : (
-      <Redirect noThrow to={redirect} />
-    );
+          </Grid.Row>
+          <Grid.Row>
+            <p className="body">{article.body}</p>
+          </Grid.Row>
+          <Grid.Row>
+            <div className="votes">Votes: {article.votes + votes} </div>
+          </Grid.Row>
+          <Grid.Row>
+            {user ? (
+              <Grid.Column computer={8} mobile={8}>
+                <Grid.Row>
+                  <Button
+                    basic
+                    color="green"
+                    disabled={votes === 1}
+                    onClick={() => this.handleVoteClick(1)}
+                  >
+                    Upvote
+                  </Button>
+                  <Button
+                    basic
+                    color="red"
+                    disabled={votes === -1}
+                    onClick={() => this.handleVoteClick(-1)}
+                  >
+                    Downvote
+                  </Button>
+                </Grid.Row>
+              </Grid.Column>
+            ) : (
+              ''
+            )}
+            {user === article.author ? (
+              <Grid.Column computer={8} mobile={8}>
+                <Button
+                  primary
+                  floated="right"
+                  onClick={() => this.handleDelete(articleId)}
+                >
+                  Delete Article
+                </Button>
+              </Grid.Column>
+            ) : (
+              ''
+            )}
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column>
+              <CommentAdder
+                articleId={articleId}
+                postedComment={this.postedComment}
+                user={user}
+              />
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Comments comments={comments} user={user} />
+          </Grid.Row>
+        </Grid>
+      ) : (
+        <Redirect noThrow to={redirect} />
+      );
+    }
   }
-  // }
 }
 
 export default Article;
