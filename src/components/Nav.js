a7ef3e8194a89c83685825b96c277e2713cbe93f
Natali Eleftheriou <nataliaelef@outlook.com>
2019-02-13 10:04:34 +0000

    Amended nav bar for mobile

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from '@reach/router';
-import { Menu, Container, Header, Dropdown } from 'semantic-ui-react';
+import { Menu, Header, Dropdown, Grid } from 'semantic-ui-react';
 import * as api from '../utils/api';
 
 class Nav extends Component {
@@ -22,51 +22,99 @@ class Nav extends Component {
   render() {
     const { user } = this.props;
     return (
-      <Container fluid className="nav-bar">
-        <Header textAlign="center" as="h1" inverted>
-          NORTHCODERS NEWS
-        </Header>
-        <Menu>
-          <Link className="nav-link" to="/">
-            <Menu.Item name="home" />
-          </Link>
-          {user && user ? (
-            <Link className="nav-link" to="/articles">
-              <Menu.Item name="articles" />
+      <Grid className="nav-bar">
+        <Grid.Row>
+          <Header textAlign="center" as="h1" inverted>
+            NORTHCODERS NEWS
+          </Header>
+        </Grid.Row>
+        <Grid.Row only="computer">
+          <Menu>
+            <Link className="nav-link" to="/">
+              <Menu.Item name="home" />
             </Link>
-          ) : (
-            ''
-          )}
-          {user && user ? (
-            <Link className="nav-link" to="/users">
-              <Menu.Item name="users" />
+            {user && user ? (
+              <Link className="nav-link" to="/articles">
+                <Menu.Item name="articles" />
+              </Link>
+            ) : (
+              ''
+            )}
+            {user && user ? (
+              <Link className="nav-link" to="/users">
+                <Menu.Item name="users" />
+              </Link>
+            ) : (
+              ''
+            )}
+            {user && user ? (
+              <Link className="nav-link" to="/topics">
+                <Menu.Item name="topics" />
+              </Link>
+            ) : (
+              ''
+            )}
+            <Link className="nav-link" to="/about">
+              <Menu.Item name="about" />
             </Link>
-          ) : (
-            ''
-          )}
-          {user && user ? (
-            <Link className="nav-link" to="/topics">
-              <Menu.Item name="topics" />
-            </Link>
-          ) : (
-            ''
-          )}
-          <Link className="nav-link" to="/about">
-            <Menu.Item name="about" />
-          </Link>
-          <Menu.Menu position="right">
-            <Menu.Item>
-              <Dropdown
-                placeholder="Select User"
-                selection
-                defaultValue={user}
-                options={this.state.usernames}
-                onChange={this.props.selectUser}
-              />
-            </Menu.Item>
-          </Menu.Menu>
-        </Menu>
-      </Container>
+            <Menu.Menu position="right">
+              <Menu.Item>
+                <Dropdown
+                  placeholder="Select User"
+                  selection
+                  defaultValue={user}
+                  options={this.state.usernames}
+                  onChange={this.props.selectUser}
+                />
+              </Menu.Item>
+            </Menu.Menu>
+          </Menu>
+        </Grid.Row>
+        <Grid.Row only="mobile">
+          <Menu vertical>
+            <Dropdown item text="Menu">
+              <Dropdown.Menu>
+                <Link className="nav-link" to="/">
+                  <Menu.Item name="home" />
+                </Link>
+                {user && user ? (
+                  <Link className="nav-link" to="/articles">
+                    <Menu.Item name="articles" />
+                  </Link>
+                ) : (
+                  ''
+                )}
+                {user && user ? (
+                  <Link className="nav-link" to="/users">
+                    <Menu.Item name="users" />
+                  </Link>
+                ) : (
+                  ''
+                )}
+                {user && user ? (
+                  <Link className="nav-link" to="/topics">
+                    <Menu.Item name="topics" />
+                  </Link>
+                ) : (
+                  ''
+                )}
+                <Link className="nav-link" to="/about">
+                  <Menu.Item name="about" />
+                </Link>
+              </Dropdown.Menu>
+            </Dropdown>
+          </Menu>
+        </Grid.Row>
+        <Grid.Row only="mobile">
+          <Dropdown
+            placeholder="Select User"
+            selection
+            defaultValue={user}
+            options={this.state.usernames}
+            onChange={this.props.selectUser}
+          />
+        </Grid.Row>
+      </Grid>
     );
   }
 }
